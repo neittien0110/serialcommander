85ed5998994ff9cd650dc75a1ffab159d676a4c4
Nguyen Duc Tien <neittien0110@gmail.com>
2025-08-07 13:01:20 +0700

    Fix-bug: thay đổi và điều chỉnh đánh số version tự động

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
+import packageJson from '../package.json';      // Lấy số hiệu version
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
   /// Thiết lập tên trang web dựa theo <---  vite.config.ts <--- package.json 
-  document.title = import.meta.env.VITE_APP_NAME + " v" + import.meta.env.VITE_APP_VERSION
+  document.title = import.meta.env.VITE_APP_NAME + " v" + packageJson.version;
   return (
     <Router basename={import.meta.env.VITE_BASE_URL}>     { /* Thiết lập basename cho các liên kết động */ }
       <AppRoutes isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
